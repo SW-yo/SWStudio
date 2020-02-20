b747492e2eda5ef5d1a4c68b9a597beaceafa207
SW-yo <sea.salt.yo@gmail.com>
2020-02-20 16:44:10 +0900

    tuple

---
+++ study_tuple.py
@@ -0,0 +1,47 @@
+"""
+a = (1, 2)
+b = ("Py", 3.6)
+c = (89, 56 ,75)
+d = ((10, 20), (30, 40))
+print(a)
+print(b)
+print(c)
+print(d)
+"""
+
+"""
+data = 1,
+print(data)
+"""
+
+"""
+a = (10, 20)
+b = (20, 30)
+c = a + b
+print(c)
+d = (40, 50)
+d += (60,)
+print(d)
+"""
+
+"""
+data = tuple(range(-5, 6))
+print(data)
+week = tuple("日月火水木金土")
+print(week)
+"""
+
+"""
+colors = ["blue", "clack", "green"]
+data = tuple(colors)
+print(data)
+data_list = list(data)
+print(data_list)
+"""
+
+
+data = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+top3 = data[:3]
+print(top3)
+skip = data[::2]
+print(skip)
